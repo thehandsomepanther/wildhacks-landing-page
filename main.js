6d3cce27756ecef87daf6e91797e8df4e3a33a97
Joshua Shi <thehandsomepanther@gmail.com>
2015-05-20 19:11:40 -0500

    added email submission and validation

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 $(document).ready(function () {
-    
+    $('#submit-button').click(function () {
+        
+        var emailInput = $('.email');
+        var emailInputValue = emailInput.val();
+        
+        if (/.+@.+\..+/.test(emailInputValue)) {
+            emailInput.val(emailInputValue.trim());
+            $('form#signup').submit();
+            
+            $.post( 'http://formula-one.herokuapp.com/wh/signup', {email: emailInputValue})
+            .done(function(result) {
+                console.log(result);
+            });
+        }
+    });
 });
